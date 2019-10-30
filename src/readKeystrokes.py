71db9e2a13871dbf910167ecca618331a2721e57
Cristina Noujaim <cristinanoujaim@Cristinas-MacBook.local>
2019-10-30 19:17:22 -0400

    first commit

---
+++ src/readKeystrokes.py
@@ -0,0 +1,10 @@
+from python_speech_features import mfcc
+from python_speech_features import logfbank
+import scipy.io.wavfile as wav
+
+
+(rate,sig) = wav.read("typing.wav")
+mfcc_feat = mfcc(sig,rate)
+fbank_feat = logfbank(sig,rate)
+
+print(fbank_feat[1:3,:])
