2c9d60c38bcf14bbb3bdc9df149ffe3a6c9d3799
Cristina Noujaim <cristinanoujaim@0587359841.wireless.umich.net>
2019-11-08 15:13:09 -0500

    use mfcc to get features

---
+++ src/learnKeystrokes.py
@@ -0,0 +1,50 @@
+
+import sys
+import os
+
+import numpy as np
+
+from librosa.feature import mfcc
+
+class ClassifyKeystrokes:
+    def __init__(self, infile):
+        self.keystrokes = np.loadtxt(infile)
+        self.Xtrain = []
+        self.convert_to_kestrokes()
+
+
+    def extract_features(self, keystroke, sr=44100, n_mfcc=16, n_fft=441, hop_len=110):
+        """Return an MFCC-based feature vector for a given keystroke."""
+        spec = mfcc(y=keystroke.astype(float),
+                    sr=sr,
+                    n_mfcc=n_mfcc,
+                    n_fft=n_fft, # n_fft=220 for a 10ms window
+                    hop_length=hop_len, # hop_length=110 for ~2.5ms
+                    )
+        return spec.flatten()
+
+
+
+    def convert_to_kestrokes(self):
+        '''Convert keystroke info to info'''
+
+        for keystroke in self.keystrokes:
+            feat = self.extract_features(keystroke)
+            self.Xtrain.append(feat)
+
+        self.Xtrain = np.stack(self.Xtrain, axis=0 )
+
+
+def main():
+    infile = str(sys.argv[1])
+    outfile = os.path.join("out", "raw_sentence", infile.split("/")[-1] + "_raw")
+
+    classifier = ClassifyKeystrokes(infile)
+
+
+
+    with open(outfile, "w") as f:
+        f.write("ehllo my namm is crissina")
+
+
+main()
