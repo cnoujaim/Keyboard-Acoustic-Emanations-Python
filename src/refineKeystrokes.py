cdee30c086c2054bd53bb0807c362e5a6afcd38f
jakehage <jakehage@umich.edu>
2019-11-13 19:18:45 -0500

    pyspellchecker very simple demo

--- src/refineKeystrokes.py
+++ src/refineKeystrokes.py
@@ -1,8 +1,11 @@
 from spell import edits2
 from spell import correction
+from spellchecker import SpellChecker
 
-misspelled = ["appel", "bananb", "orangue"]
+spell = SpellChecker()
+misspelled = ["aplpe", "bananb", "orangue"]
+
 for word in misspelled:
     print(word + ": " + correction(word))
-
+    print("Spellchecker correction: " + spell.correction(word))
 
