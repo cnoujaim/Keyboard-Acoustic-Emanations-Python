f469ea7258d169bd5c4c2fd654bf0315496347ff
jakehage <jakehage@umich.edu>
2019-11-18 19:16:11 -0500

    added n-grams function to refine

--- src/refineKeystrokes.py
+++ src/refineKeystrokes.py
@@ -1,11 +1,24 @@
+import re
+from nltk.util import ngrams
 from spell import edits2
 from spell import correction
 from spellchecker import SpellChecker
 
+
 spell = SpellChecker()
 misspelled = ["aplpe", "bananb", "orangue"]
+
 
 for word in misspelled:
     print(word + ": " + correction(word))
     print("Spellchecker correction: " + spell.correction(word))
+
+
+def generate_n_grams(word, n):
+    word = word.lower()
+    word = re.sub(r'[^a-zA-Z0-9\s]', ' ', word)
+    tokens = [token for token in word.split(" ") if token != ""]
+    return list(ngrams(tokens, n))
+
+print(generate_n_grams("Apple banana orange", 2))
 
